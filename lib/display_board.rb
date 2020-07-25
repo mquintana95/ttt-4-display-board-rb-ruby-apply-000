ec655745a4ba43611d0f4dae8134cdff38d0b4e4
Marcus Quintana <mq97325@gmail.com>
2020-07-25 21:29:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0] | #{board"
+  puts " #{board[0]} | #{board[1]} | #board"
 
